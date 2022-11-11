b1e9fe465240523365c489c16a61e75d0dd90a81
fernaag <fernando.a.lopez@ntnu.no>
2022-11-11 13:20:28 +0100

    Updated prelaude to run in one go

--- Model.py
+++ Model.py
@@ -21,8 +21,8 @@ from tqdm import tqdm
 import time
 import matplotlib
 import product_component_model as pcm
+xlrd.xlsx.ensure_elementtree_imported(False, None)
 xlrd.xlsx.Element_has_iter = True
-
 mpl_logger = log.getLogger("matplotlib")
 mpl_logger.setLevel(log.WARNING)  
 # For Ipython Notebook only
